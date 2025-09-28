107a9a183ccc3e2e4566b7b26e3db817353f32cc
Bigizic <olalekanisaac75@gmail.com>
2025-09-28 01:45:32 +0100

    add udpates

--- beauty/src/reducers.ts
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit';
-
-import service from './containers/Services/reducers'
-
-const rootReducer = combineReducers({
-  service
-})
-
-export default rootReducer;
