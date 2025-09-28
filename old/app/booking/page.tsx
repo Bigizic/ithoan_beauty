521edf17aaeeb1446019dd0414095d5be19ef911
Bigizic <olalekanisaac75@gmail.com>
2025-09-28 01:40:27 +0100

    removed all

--- old/app/booking/page.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import { BookingPage } from '../../src/screens/BookingPage';
-
-export default function Booking() {
-  return <BookingPage />;
-}
--- old/app/privacy/page.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import { PrivacyPage } from '../../src/screens/PrivacyPage';
-
-export default function Privacy() {
-  return <PrivacyPage />;
-}
